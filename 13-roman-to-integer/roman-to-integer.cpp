ca7bc5ce81c1bf4080f99e004d8bf75ce6db6ea1
Ritwik Jain <ritwik8march@gmail.com>
2026-01-25 17:01:42 +0530

    Time: 3 ms (74.54%) | Memory: 10 MB (85.60%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> val = {
-        {'I', 1}, {'V', 5}, {'X', 10},
-        {'L', 50}, {'C', 100}, {'D', 500},
-        {'M', 1000}
-    };
     int total = 0;
     int prev = 0;
     for(int i =s.size()-1;i>=0; i-- ){
-        int curr = val[s[i]];
-        if(curr<prev)total-=curr;
-        else total+=curr;
+        int curr = value(s[i]);
+        total += (curr<prev)? -curr: curr;
         prev = curr;
     }
     return total;
     }
+private:
+int value(char c) {
+    switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+    }
+    return 0;
+}
+
 };

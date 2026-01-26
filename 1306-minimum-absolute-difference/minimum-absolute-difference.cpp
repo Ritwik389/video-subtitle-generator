303d3c8e78ce688cbf41f5caf804a9956d6a701f
Ritwik Jain <ritwik8march@gmail.com>
2026-01-26 12:17:17 +0530

    Time: 11 ms (97.38%) | Memory: 36.7 MB (32.02%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -5,19 +5,18 @@ public:
         sort(arr.begin(), arr.end());
         vector<vector<int>> ans;
         for(int i =0; i<arr.size()-1; i++){
-            mindiff = min(mindiff, arr[i+1]-arr[i]);
-        }
-        for(int i =0; i<arr.size()-1; i++){
             int diff = arr[i+1] - arr[i];
-            if(diff == mindiff) ans.push_back({arr[i], arr[i+1]});
+            if(diff<mindiff){
+                mindiff = diff;
+                ans.clear();
+                ans.push_back({arr[i], arr[i+1]});
+            }
+            else if (mindiff == diff){
+                ans.push_back({arr[i], arr[i+1]});
+            }
         }
+        
         return ans;
-
-
-
-
-
-
         }
 
         
